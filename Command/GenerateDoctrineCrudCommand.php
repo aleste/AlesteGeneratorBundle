c94a6c05289ac066ea71b9ccf00f3e9a2f19771d
Alejandro Steinmetz <asteinmetz78@gmail.com>
2015-01-11 16:45:13 -0300

    Refactoring

--- Command/GenerateDoctrineCrudCommand.php
+++ Command/GenerateDoctrineCrudCommand.php
@@ -41,6 +41,63 @@ class GenerateDoctrineCrudCommand extends BaseCommand
         return new DoctrineCrudGenerator($this->getContainer()->get('filesystem'));
     }
 
+
+    /**
+     * @see Command
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $questionHelper = $this->getQuestionHelper();
+
+        if ($input->isInteractive()) {
+            $question = new ConfirmationQuestion($questionHelper->getQuestion('Do you confirm generation', 'yes', '?'), true);
+            if (!$questionHelper->ask($input, $output, $question)) {
+                $output->writeln('<error>Command aborted</error>');
+
+                return 1;
+            }
+        }
+
+        $entity = Validators::validateEntityName($input->getOption('entity'));
+        list($bundle, $entity) = $this->parseShortcutNotation($entity);
+
+        $format = Validators::validateFormat($input->getOption('format'));
+        $prefix = $this->getRoutePrefix($input, $entity);
+        $withWrite = $input->getOption('with-write');
+        $forceOverwrite = $input->getOption('overwrite');
+
+        $questionHelper->writeSection($output, 'CRUD generation');
+
+        $entityClass = $this->getContainer()->get('doctrine')->getAliasNamespace($bundle).'\\'.$entity;
+        $metadata    = $this->getEntityMetadata($entityClass);
+        $bundle      = $this->getContainer()->get('kernel')->getBundle($bundle);
+
+        $generator = $this->getGenerator($bundle);
+        $generator->generate($bundle, $bundle, $entity, $metadata[0], $format, $prefix, $withWrite, $forceOverwrite);
+
+        $output->writeln('Generating the CRUD code: <info>OK</info>');
+
+        $errors = array();
+        $runner = $questionHelper->getRunner($output, $errors);
+
+        // form
+        if ($withWrite) {
+            $output->write('Generating the Form code: ');
+            if ($this->generateForm($bundle, $entity, $metadata)) {
+                $output->writeln('<info>OK</info>');
+            } else {
+                $output->writeln('<warning>Already exists, skipping</warning>');
+            }
+        }
+
+        // routing
+        if ('annotation' != $format) {
+            $runner($this->updateRouting($questionHelper, $input, $output, $bundle, $format, $entity, $prefix));
+        }
+
+        $questionHelper->writeGeneratorSummary($output, $errors);
+    }
+
     protected function getSkeletonDirs(BundleInterface $bundle = null)
     {
         $skeletonDirs = array();
